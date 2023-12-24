9e9f2dcf85cc3e58bbbdbc586bd1ecf6db857a04
cd <lesleyrotonto@gmail.com>
2023-12-23 22:01:57 -0500

    added user schema with email validation ;

---
+++ models/userModels.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    //define the schema for user
+    userName: {type: String, uinque: true, required: true},
+    email: {type: String, uinque: true, required: true, validate: {
+        validator: function(value){
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+         },
+         message: 'Invalid email address format',
+        },
+    },
+
+})
+
